5e0f2b2d4e36ebab86ad903ee150c273de889e65
marabidze <marabidze@slb.com>
2023-09-09 15:15:52 +0400

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomelessCatalogComponent } from './pages/homeless-catalog/homeless-catalog.component';
+import { AdoptedCatalogComponent } from './pages/adopted-catalog/adopted-catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'home',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'homeless-catalog',
+    component: HomelessCatalogComponent,
+  },
+  {
+    path: 'adopted-catalog',
+    component: AdoptedCatalogComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
