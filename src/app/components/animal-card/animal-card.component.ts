e56a7636547889e7fa9aee8539a77f2695ffa362
marabidze <marabidze@slb.com>
2023-09-09 19:42:01 +0400

    feat: add anima-card content

--- src/app/components/animal-card/animal-card.component.ts
+++ src/app/components/animal-card/animal-card.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-animal-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './animal-card.component.html',
-  styleUrls: ['./animal-card.component.scss']
+  styleUrls: ['./animal-card.component.scss'],
 })
-export class AnimalCardComponent {
-
-}
+export class AnimalCardComponent {}
