865ef073fb9c6cd959cc314e06ff06d5ab7dcaa9
marabidze <marabidze@slb.com>
2023-09-09 21:06:56 +0400

    fix: animal card

--- src/app/components/animal-card/animal-card.component.ts
+++ src/app/components/animal-card/animal-card.component.ts
@@ -1,14 +1,24 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { Animal } from 'src/app/models/animal';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-animal-card',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './animal-card.component.html',
   styleUrls: ['./animal-card.component.scss'],
 })
-export class AnimalCardComponent {}
+export class AnimalCardComponent {
+  @Input() animal!: Animal;
+}
